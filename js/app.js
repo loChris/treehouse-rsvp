8044011e29bb8335b3f94c1f5877489ca33d4ad1
loChris <44450518+loChris@users.noreply.github.com>
2020-11-06 18:14:22 -0400

    Added functionality to the save button

--- js/app.js
+++ js/app.js
@@ -54,17 +54,26 @@ ul.addEventListener('click', (event) => {
 		const button = event.target;
 		const li = button.parentNode;
 		const ul = li.parentNode;
-		const span = li.firstElementChild;
-		const input = document.createElement('input');
 
 		if (button.textContent === 'remove') {
 			ul.removeChild(li);
 		} else if (button.textContent === 'edit') {
+			const span = li.firstElementChild;
+			const input = document.createElement('input');
+
 			input.type = 'text';
 			input.value = span.textContent;
 			li.insertBefore(input, span);
 			li.removeChild(span);
 			button.textContent = 'save';
+		} else if (button.textContent === 'save') {
+			const input = li.firstElementChild;
+			const span = document.createElement('span');
+
+			span.textContent = input.value;
+			li.insertBefore(span, input);
+			li.removeChild(input);
+			button.textContent = 'edit';
 		}
 	}
 });
